7f7c8e3d2f67a17f7bded6d52cfd62706121ef8a
AedanCG <aedangarnand@gmail.com>
2018-09-07 11:11:59 -0700

    Toggle

--- query.js
+++ query.js
@@ -1,2 +1,20 @@
 $(document).ready(function() {
+	$("h1").click(function(){
+		//$("p").toggle();
+		$("p").css({"color":"red", "background-color":"yellow"});
+		$("img").css({"border":"2px solid red"});
+	});
+	
+	$(".p1").click(function(){
+		$(".p2").toggle();
+		//$("img").toggle();
+	});
+	
+	$(".p2").click(function(){
+		$(".p3").toggle();
+	});
+	
+	$(".p3").click(function(){
+		$(".p4").toggle();
+	});
 });
